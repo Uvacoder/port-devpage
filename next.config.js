75bef23b9f7a1981494b0950a17ab897018ab8e5
Alexander May <alexander.may@spendit.de>
2021-10-27 08:43:34 +0200

    update packages, add csp headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,43 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const { i18n } = require("./next-i18next.config");
+
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Content-Security-Policy",
+    value: "object-src 'none'; base-uri 'none';",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "strict-origin-when-cross-origin",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "microphone=(),geolocation=*",
+  },
+];
+
 module.exports = {
+  swcMinify: true,
   i18n,
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
